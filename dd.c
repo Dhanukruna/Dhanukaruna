be2a9422e6f5cd435f32dc1de3fff9a3c7483b7e
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2016-12-21 11:36:24 +0530

    Add files via upload

---
+++ dd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+        printf("\n The number is positive",a);
+    else
+        printf("\n The number is negative",a);
+}
