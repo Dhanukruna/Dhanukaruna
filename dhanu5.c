480a87f5cbbf9df2a7b9a3115e02baea4343aab7
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2016-12-28 22:19:18 +0530

    Add files via upload

---
+++ dhanu5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char str= {'a','e','i','o','u'},n;
+    printf("\nEnter the character ");
+    scanf("%c",&n);
+    if(n==str){
+        printf("\n Vowel ");
+    }
+    else
+        printf("\nConsonant");
+
+
+}
