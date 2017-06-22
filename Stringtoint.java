510a318f0f5e0a037059ec77951cb334da24ec4c
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2017-06-22 14:20:08 +0530

    Add files via upload

---
+++ Stringtoint.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.lang.String;
+import java.util.Scanner;
+public class Stringtoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		int a=Integer.parseInt(s1);
+		System.out.println(a);
+		sc.close();
+	}
+
+}
