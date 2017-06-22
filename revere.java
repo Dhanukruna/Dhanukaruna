32aef0f2340f419fc2f3e3f65e1d96abcd2391bf
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2017-06-22 11:32:17 +0530

    Add files via upload

--- revere.java
+++ revere.java
@@ -1,10 +1,16 @@
 package guvi;
-
+import java.util.Scanner;
 public class revere {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		int temp=0;
+		for(int a=s1.length()-1;a>=temp;a--){
+			System.out.print(s1.charAt(a));
+		}
+		
+		sc.close();
 	}
-
 }
