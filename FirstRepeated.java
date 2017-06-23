74f2df7f6e5725026837e459773c102cbb92c6b9
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2017-06-23 14:51:03 +0530

    Add files via upload

--- FirstRepeated.java
+++ FirstRepeated.java
@@ -5,7 +5,7 @@ public class FirstRepeated {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		int n=input.nextInt();
-		int count=0;
+		int count;
 		int[] a=new int[n];
 		for(int i=0;i<n;i++){
 			a[i]=input.nextInt();
@@ -14,11 +14,14 @@ public class FirstRepeated {
 			count=0;
 			for(int j=i+1;j<n;j++){
 				if(a[i]==a[j]){
-					System.out.println("The First Repeated Element in the Array is"+a[i]);
+					count++;
+				}
+				if(count==1){
+					System.out.println(a[i]);
 				}
 				
-				
 			}
+			
 		}
 		
 		input.close();
