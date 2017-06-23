cea7a59715906773539806e3777c68dd2db6eb4a
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2017-06-23 14:52:53 +0530

    Add files via upload

---
+++ Frepeated.java
@@ -0,0 +1,30 @@
+package guvi;
+import java.util.Scanner;
+public class Frepeated {
+
+		public static void main(String[] args) {
+			Scanner input=new Scanner(System.in);
+			int n=input.nextInt();
+			int count;
+			int[] a=new int[n];
+			for(int i=0;i<n;i++){
+				a[i]=input.nextInt();
+			}
+			for(int i=0;i<n;i++){
+				count=0;
+				for(int j=i+1;j<n;j++){
+					if(a[i]==a[j]){
+						count++;
+					}
+					if(count==1){
+						System.out.println(a[i]);
+					}
+					
+				}
+				
+			}
+			
+			input.close();
+		}
+
+}
