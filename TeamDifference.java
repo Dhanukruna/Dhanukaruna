49d0e304c23e180d87d6a63fcb90cc39f07eff3b
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2017-06-30 12:25:45 +0530

    Add files via upload

---
+++ TeamDifference.java
@@ -0,0 +1,24 @@
+package guvi;
+import java.util.Scanner;
+public class TeamDifference {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Input Weights:");
+		int a=s.nextInt();
+		int rem=a/2;
+		int diff=a-rem;
+		int b=diff-rem;
+		System.out.print(diff);
+		System.out.println(" "+rem);
+		if(rem==diff){
+			System.out.println("Output"+b);
+		}
+		else{
+			System.out.println("Output"+b);
+		}
+		s.close();
+
+	}
+
+}
