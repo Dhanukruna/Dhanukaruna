da073a3ba25b33833eca9a43aeef8b79978f5440
Dhanukruna <dhanushyaakarunakaran96@gmail.com>
2017-08-03 12:44:52 +0530

    Add files via upload

--- ReverseWord.java
+++ ReverseWord.java
@@ -1,19 +1,13 @@
 package guvi;
 import java.util.Scanner;
 public class ReverseWord {
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			Scanner s=new Scanner(System.in);
-			String a=s.nextLine();
-			String s1="";
-			String[] b=a.split(" ");
-			for(int i=b.length-1;i>=0;i--){
-				s1+=b[i]+" ";
-				
-			}
-			System.out.println(s1);
-	s.close();
-		}
-
+public  static void main(String args[]){
+	Scanner s=new Scanner(System.in);
+	String a=s.nextLine();
+	String[] b=a.split(" ");
+	for(int i=b.length-1;i>=0;i--){
+		System.out.print(b[i]+" ");
 	}
-                 
+	s.close();
+}
+}
